269c9b2c4d3768da924419beffefd2f669a9b6d7
MarcinGarcin <knucias123@gmail.com>
2024-10-07 18:33:11 +0200

    Created basic code structure

---
+++ src/main/java/user/user.java
@@ -0,0 +1,16 @@
+package user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public record user(
+        @Id
+        Integer id,
+        String login,
+        String password
+) {
+
+}
