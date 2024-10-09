3b5ec898400208a8e51737ca23c9e437f5ed404b
MarcinGarcin <knucias123@gmail.com>
2024-10-09 23:37:28 +0200

    custom html login, working connection with database

--- src/main/java/MarcinGarcin/ToDoApp/Security/SecurityConfig.java
+++ src/main/java/MarcinGarcin/ToDoApp/Security/SecurityConfig.java
@@ -1,31 +1,57 @@
 package MarcinGarcin.ToDoApp.Security;
 
 
+import MarcinGarcin.ToDoApp.user.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 
 @Configuration
+@AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private final UserService userService;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .formLogin(httpForm ->{
-                    httpForm
-                            .loginPage("/login").permitAll();
-                }).authorizeHttpRequests(registry ->{
-                        registry.requestMatchers("/req/signup","/css/**","").permitAll();
-                        registry.anyRequest().authenticated();
-                })
-
-
-
-                .build();
+    public UserDetailsService userDetailsService() {
+        return userService;
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider  authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
+    }
+
 }
---
+++ src/main/java/MarcinGarcin/ToDoApp/user/UserService.java
@@ -0,0 +1,40 @@
+package MarcinGarcin.ToDoApp.user;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByUsername(login);
+
+        if (user.isPresent()) {
+
+            var userObj = user.get();
+
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .build();
+
+        }
+        else{
+            throw new UsernameNotFoundException(login);
+        }
+
+    }
+}
